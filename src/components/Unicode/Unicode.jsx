e4301eaca1510386cc0c9d3da225cbcd40eababc
wies同学 <104070826+wieszheng@users.noreply.github.com>
2023-11-03 13:01:23 +0800

    Add files via upload

---
+++ src/components/Unicode/Unicode.jsx
@@ -0,0 +1,65 @@
+import { Col, Input, Row, Tabs } from 'antd';
+import React, { useState } from 'react';
+import { ToGB2312, ToUnicode } from '../../utils/unicode';
+import { SortAscendingOutlined, SortDescendingOutlined } from '@ant-design/icons';
+const { TabPane } = Tabs;
+const { TextArea } = Input;
+
+const Unicode = () => {
+  const [chinese, setChinese] = useState('');
+  const [unicode, setUnicode] = useState('');
+  return (
+    <Tabs defaultActiveKey="1" style={{ width: '100%' }}>
+      <TabPane
+        tab={
+          <span>
+            <SortAscendingOutlined />
+            Unicode To Chinese
+          </span>
+        }
+        key="1"
+      >
+        <Row gutter={[30, 8]} style={{ padding: '24px 24px' }}>
+          <Col span={12}>
+            <TextArea
+              rows={10}
+              placeholder="Unicode 字符集"
+              onChange={(e) => {
+                setChinese(e.target.value);
+              }}
+            />
+          </Col>
+          <Col span={12}>
+            <TextArea rows={10} placeholder="Chinese 中文" value={ToGB2312(chinese)} />
+          </Col>
+        </Row>
+      </TabPane>
+      <TabPane
+        tab={
+          <span>
+            <SortDescendingOutlined />
+            Chinese To Unicode
+          </span>
+        }
+        key="2"
+      >
+        <Row gutter={[30, 8]} style={{ padding: '24px 24px' }}>
+          <Col span={12}>
+            <TextArea
+              rows={10}
+              placeholder="Chinese 中文"
+              onChange={(e) => {
+                setUnicode(e.target.value);
+              }}
+            />
+          </Col>
+          <Col span={12}>
+            <TextArea rows={10} placeholder="Unicode 字符集" value={ToUnicode(unicode)} />
+          </Col>
+        </Row>
+      </TabPane>
+    </Tabs>
+  );
+};
+
+export default Unicode;
