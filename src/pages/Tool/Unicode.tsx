6602f6856ccc62dff732a956a991c878d38edb69
wies同学 <104070826+wieszheng@users.noreply.github.com>
2023-11-03 13:06:35 +0800

    Update Unicode.tsx

--- src/pages/Tool/Unicode.tsx
+++ src/pages/Tool/Unicode.tsx
@@ -1,1 +1,12 @@
-import Unicode from './components/Unicode/Unicode';
+import React from 'react';
+import Unicode from '@/components/Postman/Unicode';
+
+
+const Request: React.FC = () => {
+
+  return (
+    <Unicode/>
+  );
+};
+
+export default Unicodes;
