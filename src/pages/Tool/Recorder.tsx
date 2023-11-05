c4ee8c9a80cbcff54fc583663db9e24a64dc2822
wies同学 <104070826+wieszheng@users.noreply.github.com>
2023-11-05 23:35:59 +0800

    Add files via upload

---
+++ src/pages/Tool/Recorder.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ScreenCap from "@/components/Recorder/ScreenCap";
+
+
+const Recorder: React.FC = () => {
+
+  return (
+    <ScreenCap/>
+  );
+};
+
+export default Recorder;
